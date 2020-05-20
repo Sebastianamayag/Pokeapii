4401a843d8683b86204d32bcc112b82b9aa87699
santiago alejandro <santi20alejo@hotmail.com>
2020-05-20 18:45:55 -0500

    ver 1

---
+++ pokeapi/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Json;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System;
+
+namespace pokeapi
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            EditText numpoke = FindViewById<EditText>(Resource.Id.NumpokeText);
+            Button button = FindViewById<Button>(Resource.Id.getPokemonButton);
+            button.Click += async (sender, e) => {
+
+                // Get the latitude and longitude entered by the user and create a query.
+                string url = "https://pokeapi.co/api/v2/pokemon/"+numpoke.Text+"/";
+
+                // Fetch the weather information asynchronously, 
+                // parse the results, then update the screen:
+                JsonValue json = await FetchPokemonAsync(url);
+                ParseAndDisplay (json);
+            };
+
+        }
+        private async Task<JsonValue> FetchPokemonAsync(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
+        }
+
+        private void ParseAndDisplay(JsonValue json)
+        {
+            // Get the weather reporting fields from the layout resource:
+            TextView Name = FindViewById<TextView>(Resource.Id.nameText);
+            TextView Url = FindViewById<TextView>(Resource.Id.urlText);
+
+            // Extract the array of name/value results for the field name "forms". 
+            JsonValue forms = json["forms"];
+
+            // Extract the "stationName" (location string) and write it to the location TextBox:
+            Name.Text = forms["name"];
+            Url.Text = forms["url"];
+
+
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
